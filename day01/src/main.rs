97c552fed7570e80aad1e47753b20effef4b9872
Alexander Ray <alexander.ray@charter.com>
2021-12-20 12:07:43 -0700

    Add gitignore

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+
+
+fn read_ints<R: Read>(io: R) -> Result<Vec<usize>, Error> {
+    let br = BufReader::new(io);
+    let mut values = vec![];
+    for line in br.lines() {
+        values.push(line?
+            .trim()
+            .parse()
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?);
+    }
+    Ok(values)
+}
+
+fn main() -> Result<(), Error> {
+    // let input_path = PathBuf::from("day01.txt");
+    let puzzle_input = read_ints(File::open("day01.txt")?)?;
+    Ok(())
 }
