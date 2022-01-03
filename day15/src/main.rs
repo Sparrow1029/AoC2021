ade57bf70da11f38265d53d0cd93b394f730b879
Alexander Ray <alexander.ray@charter.com>
2022-01-03 16:41:59 -0700

    day 15 - added doc comments; some cleanup

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -46,6 +46,13 @@ fn a_star_search(graph: &WeightedGraph, start: Coord, goal: Coord) -> (PathMap, 
         }
 
         // Visit all E W N S neighbors of the grid
+        // get the total cost so far to visit this node
+        // if the neighbor's total cost is not in the CostMap
+        // OR it has been visited before, and its new edge cost is less than its previous value
+        // - insert it into the CostMap
+        // - tag it with a priority of its total_cost + cost returned by the heuristic
+        // - put it into the `priority_queue::DoublePriorityQueue`
+        // - add the new k, v pair to the PathMap
         for next in graph.neighbors(current) {
             let new_cost = cost_so_far.get(&current).unwrap() + graph.cost(current, next);
             if !cost_so_far.contains_key(&next) || new_cost < *cost_so_far.get(&next).unwrap() {
@@ -61,6 +68,10 @@ fn a_star_search(graph: &WeightedGraph, start: Coord, goal: Coord) -> (PathMap, 
     (came_from, cost_so_far)
 }
 
+/// Reconstruct the lowest-cost path found by the A* search.
+///
+/// Iterates through the `HashMap` of (x, y) -> (x, y) pairs (goal -> start)
+/// then reverses the path so it is returned (start -> goal)
 fn reconstruct_path(came_from: &PathMap, start: Coord, goal: Coord) -> Vec<Coord> {
     let mut current = goal;
     let mut path = vec![];
@@ -73,12 +84,15 @@ fn reconstruct_path(came_from: &PathMap, start: Coord, goal: Coord) -> Vec<Coord
     path
 }
 
+/// The heuristic for the 2-D `WeightedGrid` (N,E,S,W) is the manhattan distance from the
+/// current (x, y) coordinate to the goal (x, y). Shorter is better (min value)
 fn heuristic(a: Coord, b: Coord) -> f64 {
     let (x1, y1) = a;
     let (x2, y2) = b;
     (x1 - x2).abs() as f64 + (y1 - y2).abs() as f64
 }
 
+/// Graph struct with width, height, and a map of (x, y) coordinates -> weight value
 #[derive(Debug)]
 struct WeightedGraph {
     width: isize,
@@ -87,6 +101,7 @@ struct WeightedGraph {
 }
 
 impl WeightedGraph {
+    /// convenience function to return a value from `self.weights`
     fn get(&self, id: Coord) -> Option<f64> {
         match self.weights.get(&id) {
             Some(result) => Some(*result),
@@ -94,10 +109,14 @@ impl WeightedGraph {
         }
     }
 
+    /// Get the (x, y) coordinate representing the bottom-right corner of the grid
     fn bottom_right(&self) -> Coord {
         (self.width-1, self.height-1)
     }
 
+    /// Added the twist to the cost (though mostly unecessary for AoC2021 graph input) from
+    /// Redblob games' implementation [here](https://www.redblobgames.com/pathfinding/a-star/implementation.html#troubleshooting-ugly-path)
+    /// helps make paths "pretty"; even though the cost is the lowest, it may not be the visually/logically "straightest" path to a human.
     fn cost(&self, from_node: Coord, to_node: Coord) -> f64 {
         let prev_cost = *self.weights.get(&to_node).unwrap_or(&1.0);
         let mut nudge = 0.0;
@@ -111,11 +130,13 @@ impl WeightedGraph {
         prev_cost + 0.001 * nudge
     }
 
+    /// Determine if a coordinate (x, y) is in the bounds of the Grid. Used by `neighbors` function.
     fn in_bounds(&self, id: Coord) -> bool {
         let (x, y) = id;
         0 <= x && x < self.width as isize && 0 <= y && y < self.height as isize
     }
 
+    /// Return all E, W, N, S neighbors for a given coordinate (x, y) point.
     fn neighbors(&self, id: Coord) -> Vec<Coord> {
         let (x, y) = id;
         let mut neighbors: Vec<Coord> = Vec::from([(x + 1, y), (x -1, y), (x, y - 1), (x, y + 1)]);
@@ -128,6 +149,7 @@ impl WeightedGraph {
         neighbors
     }
 
+    /// Print a visual representation of the grid with weights.
     fn display(&self) {
         for y in 0..self.height {
             for x in 0..self.width {
@@ -137,9 +159,46 @@ impl WeightedGraph {
         }
     }
 
-    /// Expand grid as tiles by factor of `factor` and according to heuristic:
+    /// Similar to `display`, except with terminal color codes:
+    /// - Cyan      - A visited node in the least-cost path
+    /// - Dark Grey - A visited node NOT in the least-cost path
+    /// - White     - just a regular old node
+    fn display_with_path(&self, path: &Vec<Coord>, costs: &CostMap) {
+        let visited_nodes = costs.iter().map(|t| *t.0).collect::<Vec<Coord>>();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let color = if path.contains(&(x, y)) {
+                    "\x1b[1;36m"
+                } else if visited_nodes.contains(&(x, y)) {
+                    "\x1b[1;30m"
+                } else {
+                    ""
+                };
+                print!("{}{}\x1b[0m", color, self.get((x, y)).unwrap());
+            }
+            println!();
+        }
+    }
+
+    /// Expand grid as tiles by factor of `factor`.
+    /// For each expanded tile, weights increase by one unless they are > 9.0, which resets weight to 1.0.
     ///
-    /// for each expanded tile, weights increase by one unless they are > 9.0, which resets weight to 1.0.
+    /// Grid tile
+    /// ```
+    /// 1 4 8
+    /// 1 1 1
+    /// 3 9 1
+    /// ```
+    /// `expand`ed by a factor of 2 Becomes
+    /// ```
+    /// 1 4 8 | 2 5 9
+    /// 1 1 1 | 2 2 2
+    /// 3 9 1 | 4 1 2
+    /// -------------
+    /// 2 5 9 | 3 6 1
+    /// 2 2 2 | 3 3 3
+    /// 4 1 2 | 5 2 3
+    /// ```
     fn expand(&mut self, factor: isize) {
         // Expand horizontally first
         for y in 0..self.height {
@@ -179,23 +238,6 @@ impl WeightedGraph {
         }
         self.height *= factor;
     }
-
-    fn display_with_path(&self, path: &Vec<Coord>, costs: &CostMap) {
-        let visited_nodes = costs.iter().map(|t| *t.0).collect::<Vec<Coord>>();
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let color = if path.contains(&(x, y)) {
-                    "\x1b[1;36m"
-                } else if visited_nodes.contains(&(x, y)) {
-                    "\x1b[1;30m"
-                } else {
-                    ""
-                };
-                print!("{}{}\x1b[0m", color, self.get((x, y)).unwrap());
-            }
-            println!();
-        }
-    }
 }
 
 /// Get the coordinate for the lower right-hand corner of the grid
@@ -205,6 +247,7 @@ fn get_max_xy(pts: &Vec<Coord>) -> Coord {
     (max_x, max_y)
 }
 
+/// Parse input file into a `WeightedGraph` struct
 fn parse_input(path: &str) -> WeightedGraph {
     // let mut graph = HashMap::new();
     let mut pt_vec: Vec<((isize, isize), f64)> = vec![];
@@ -226,6 +269,7 @@ fn parse_input(path: &str) -> WeightedGraph {
     }
 }
 
+/// Unused function to determine cost of all nodes visited, but kept the code around...
 // fn get_total_cost(path: &Vec<Coord>, costs: &CostMap) -> f64 {
 //     let mut sum = 0.0;
 //     path.iter().for_each(|pt| sum += costs.get(pt).unwrap());
@@ -246,10 +290,11 @@ fn main() {
     // Part 2
     graph.expand(5);
     let goal = graph.bottom_right();
-    let (came_from, costs) = a_star_search(&graph, (0, 0), goal);
+    let (_, costs) = a_star_search(&graph, (0, 0), goal);
+    // YIKES... The `.display_with_path` function was toooo heavy for a 500 x 500 grid...
+    // would need to implement a more efficient lookup for visited nodes, etc
     // let reconstructed = reconstruct_path(&came_from, (0, 0), goal);
-    // graph.display_with_path(&reconstructed, &costs);  // YIKES... would need to implement a more efficient lookup for visited nodes, etc
+    // graph.display_with_path(&reconstructed, &costs);
     let final_cost = costs.get(&graph.bottom_right()).unwrap().round();
     println!("Final cost {:?}", final_cost);
-
 }
